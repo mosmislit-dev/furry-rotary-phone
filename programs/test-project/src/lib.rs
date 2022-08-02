f70350b3e8c41359e3e0f093672ac90e71af1958
amiljus <amiljus@protonmail.com>
2022-08-02 18:22:06 +0200

    initial commit

---
+++ programs/test-project/src/lib.rs
@@ -0,0 +1,149 @@
+use anchor_lang::prelude::*;
+use anchor_lang::solana_program::{program::invoke, system_instruction::transfer};
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+// https://book.anchor-lang.com/anchor_references/space.html.
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+const BIT_8_LENGTH: usize = 1;
+const BIT_64_LENGTH: usize = 8;
+const FLOAT_64_LENGTH: usize = 8;
+const ENUM_LENGTH: usize = 1 + 1;
+
+#[program]
+pub mod test_project {
+    use super::*;
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        default_time_increase_per_bet: i64,
+        minimal_time_increase_per_bet: i64,
+        auction_duration: i64,
+        max_participation_amount: u64,
+        min_pot_size: u64,
+    ) -> Result<()> {
+        let state_account: &mut Account<State> = &mut ctx.accounts.auction_instance;
+        let vault: &mut Account<Vault> = &mut ctx.accounts.vault;
+
+        state_account.vault_public_key = ctx.accounts.vault.key().clone();
+
+        state_account.auction_state = AuctionState::ENDED;
+
+        state_account.default_time_increase_per_bet = default_time_increase_per_bet;
+        state_account.minimal_time_increase_per_bet = minimal_time_increase_per_bet;
+        state_account.auction_duration = auction_duration;
+        state_account.max_participation_amount = max_participation_amount;
+        state_account.min_pot_size = min_pot_size;
+
+        state_account.time_decrease_per_bet_reduction_speed = 1;
+
+        state_account.treasury_to_pot_size_percentage = 0.1;
+        state_account.pot_size_to_participation_amount_percentage = 0.01;
+        state_account.pot_size_fee_percentage = 0.09;
+
+        Ok(())
+    }
+
+    pub fn fund_vault(ctx: Context<FundVault>) -> Result<()> {
+        let payer = &mut ctx.accounts.payer;
+        let receiver = &mut ctx.accounts.receiver;
+        let amount: u64 = 1_000_000_000;
+
+        if payer.lamports() < amount {
+            return Err(error!(ErrorCode::NotEnoughSol));
+        }
+
+        let instruction = transfer(&payer.key(), &receiver.key(), amount);
+
+        let account_infos = [
+            payer.to_account_info().clone(),
+            receiver.to_account_info().clone(),
+            ctx.accounts.system_program.to_account_info().clone(),
+        ];
+
+        invoke(&instruction, &account_infos)?;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, seeds = [b"vault"], bump, space = Vault::LEN)]
+    pub vault: Account<'info, Vault>,
+    #[account(init, payer = user, space = State::LEN)]
+    pub auction_instance: Account<'info, State>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct FundVault<'info> {
+    /// CHECK:
+    #[account(mut)]
+    pub receiver: AccountInfo<'info>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct State {
+    default_time_increase_per_bet: i64,
+    minimal_time_increase_per_bet: i64,
+    auction_duration: i64,
+    max_participation_amount: u64,
+    min_pot_size: u64,
+    auction_state: AuctionState,
+    time_decrease_per_bet_reduction_speed: u64,
+    // ongoing auction, to own account?
+    current_auction_start_time: i64,
+    current_auction_starting_pot_size: u64,
+    current_auction_number: u64,
+    current_participation_amount: u64,
+    // fees
+    treasury_to_pot_size_percentage: f64,
+    pot_size_to_participation_amount_percentage: f64,
+    pot_size_fee_percentage: f64,
+    // current winner
+    current_winning_bid_made_at: i64,
+    current_winning_pubkey: Pubkey,
+    //
+    vault_public_key: Pubkey,
+}
+
+#[account]
+pub struct Vault {
+    bump: u8,
+}
+
+impl Vault {
+    const LEN: usize = DISCRIMINATOR_LENGTH + BIT_8_LENGTH;
+}
+
+// 3. Add a constant on the Tweet account that provides its total size.
+impl State {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + 5 * TIMESTAMP_LENGTH
+        + 6 * BIT_64_LENGTH
+        + 3 * FLOAT_64_LENGTH
+        + ENUM_LENGTH
+        + 2 * PUBLIC_KEY_LENGTH;
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq)]
+enum AuctionState {
+    STARTED,
+    ENDED,
+    LOTTERY,
+    WAITING,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Not enough SOL")]
+    NotEnoughSol,
+}
---
+++ tests/test-project.ts
@@ -0,0 +1,136 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { TestProject } from "../target/types/test_project";
+
+const timeIncreasePerBetInSeconds = 60;
+const minimalTimeIncreasePerBetInSeconds = 24;
+const auctionDurationInSeconds = 15 * 60;
+const maxParticipationAmount = 100000;
+const minPotSize = 100000;
+
+describe("test-project", () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.TestProject as Program<TestProject>;
+  let pdaVaultPublicKey: anchor.web3.PublicKey, stateBump: number;
+
+  beforeEach(async () => {
+    [pdaVaultPublicKey, stateBump] =
+      await anchor.web3.PublicKey.findProgramAddress(
+        [Buffer.from("vault")],
+        program.programId,
+      );
+  });
+
+  it("Is initialized!", async () => {
+    const a = await provider.connection.getAccountInfo(
+      provider.wallet.publicKey,
+    );
+    console.log(pdaVaultPublicKey);
+    console.log(a.owner);
+
+    const tx = await program.methods
+      .initialize(
+        new anchor.BN(timeIncreasePerBetInSeconds),
+        new anchor.BN(minimalTimeIncreasePerBetInSeconds),
+        new anchor.BN(auctionDurationInSeconds),
+        new anchor.BN(maxParticipationAmount),
+        new anchor.BN(minPotSize),
+      )
+      .accounts({ vault: pdaVaultPublicKey, user: provider.publicKey })
+      .rpc();
+
+    const b = await provider.connection.getAccountInfo(
+      provider.wallet.publicKey,
+    );
+    console.log(b.owner);
+
+    // const programState = await program.account.state.fetch(
+    //   program.provider.publicKey,
+    // );
+
+    // const vault = await program.account.vault.fetch(
+    //   programState.vaultPublicKey,
+    // );
+
+    // const userAccountInfo = await provider.connection.getBalance(
+    //   provider.publicKey,
+    // );
+
+    // let txFund = new anchor.web3.Transaction();
+
+    // txFund.add(
+    //   anchor.web3.SystemProgram.transfer({
+    //     fromPubkey: newUser.publicKey,
+    //     toPubkey: provider.wallet.publicKey,
+    //     lamports: 1 * anchor.web3.LAMPORTS_PER_SOL,
+    //   }),
+    // );
+
+    // await anchor.web3.sendAndConfirmTransaction(provider.connection, txFund, [
+    //   newUser,
+    // ]);
+  });
+
+  it("it is funded", async () => {
+    const newUser = anchor.web3.Keypair.generate();
+
+    let airdropSig = await provider.connection.requestAirdrop(
+      newUser.publicKey,
+      5 * anchor.web3.LAMPORTS_PER_SOL,
+    );
+
+    const latestBlockHash = await provider.connection.getLatestBlockhash();
+
+    await provider.connection.confirmTransaction({
+      blockhash: latestBlockHash.blockhash,
+      lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+      signature: airdropSig,
+    });
+
+    const providerBalanceBefore = await provider.connection.getBalance(
+      provider.wallet.publicKey,
+    );
+    const userBalanceBefore = await provider.connection.getBalance(
+      newUser.publicKey,
+    );
+
+    console.log({
+      providerBalanceBefore,
+      userBalanceBefore,
+    });
+
+    const a = await provider.connection.getAccountInfo(
+      provider.wallet.publicKey,
+    );
+    console.log(a.owner);
+
+    const tx = await program.methods
+      .fundVault()
+      .accounts({
+        payer: newUser.publicKey,
+        receiver: provider.wallet.publicKey,
+      })
+      .signers([newUser])
+      .rpc();
+
+    console.log("Your transaction signature", tx);
+
+    const b = await provider.connection.getAccountInfo(
+      provider.wallet.publicKey,
+    );
+    console.log(b.owner);
+
+    const providerBalanceAfter = await provider.connection.getBalance(
+      provider.wallet.publicKey,
+    );
+    const userBalanceAfter = await provider.connection.getBalance(
+      newUser.publicKey,
+    );
+
+    console.log({
+      providerBalanceAfter,
+      userBalanceAfter,
+    });
+  });
+});
